6707d1aa6c2207c61d53f37869bd9da839845087
sergio esteban torres <myplancash@gmail.com>
2024-02-25 09:53:08 -0500

    globals.css file modified

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,22 @@
-import Sidebar from '@/components/shared/Sidebar'
 import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from '@/components/shared/Sidebar'
+// import { Toaster } from '@/components/ui/toaster'
 
- 
-const Layout = ({children} : {children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
+    <main className="root">
       <Sidebar />
       <MobileNav />
 
-      <div className='root-container'>
-        <div className='wrapper'>
+      <div className="root-container">
+        <div className="wrapper">
           {children}
         </div>
       </div>
+      
+      {/* <Toaster /> */}
     </main>
   )
 }
 
-export default Layout;
+export default Layout
